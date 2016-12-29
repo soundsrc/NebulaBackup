855bd34b9f1235cc830fe180115d6ca4d42c15d1
Sound <sound@sagaforce.com>
2016-12-29 11:24:52 -0800

    AsyncProgress implemented with first design.

--- libnebula/AsyncProgress.h
+++ libnebula/AsyncProgress.h
@@ -18,6 +18,9 @@
 
 #include <string>
 #include <memory>
+#include <mutex>
+#include <condition_variable>
+#include "ErrorCodes.h"
 
 namespace Nebula
 {
@@ -25,17 +28,200 @@ namespace Nebula
 	class AsyncProgressData
 	{
 	public:
-		bool isReady() const;
-		bool hasError() const;
+		AsyncProgressData()
+		: mIsDone(false)
+		, mIsReady(false)
+		, mCancelRequested(false)
+		, mProgress(0.0f)
+		, mErrorCode(ErrorCode::NoError)
+		, mOnDoneCallback(nullptr)
+		, mOnCancelledCallback(nullptr)
+		, mProgressCallback(nullptr)
+		, mOnErrorCallback(nullptr) { }
 
-		float progress() const;
+		void setReady(const T& result)
+		{
+			std::lock_guard<std::mutex> lock(mMutex);
+			mResult = result;
+			mIsReady = true;
+			mIsDone = true;
+			mDoneCondition.notify_all();
+		}
 		
-		std::string errorMessage() const;
+		void setError(ErrorCode errorCode, const std::string& errorMessage)
+		{
+			std::lock_guard<std::mutex> lock(mMutex);
+			mErrorCode = errorCode;
+			mErrorMessage = errorMessage;
+			mIsDone = true;
+			mDoneCondition.notify_all();
+			
+			if(mOnErrorCallback) {
+				mOnErrorCallback(errorCode, errorMessage);
+			}
+		}
+
+		bool cancelRequested() { return mCancelRequested; }
+
+		void cancel()
+		{
+			mCancelRequested = true;
+		}
+		
+		void setCancelled()
+		{
+			{
+				std::lock_guard<std::mutex> lock(mMutex);
+				if(mOnCancelledCallback) {
+					mOnCancelledCallback();
+				}
+			}
+			setError(ErrorCode::UserCancelled, "User cancelled.");
+		}
+
+		void setProgress(float progress)
+		{
+			std::lock_guard<std::mutex> lock(mMutex);
+			if(progress > mProgress) {
+				mProgress = progress;
+				if(mProgressCallback) mProgressCallback(mProgress);
+			}
+		}
+		
+		bool isDone() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mIsDone;
+		}
+
+		bool isReady() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mIsReady;
+		}
+		
+		float progress() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mProgress;
+		}
+		
+		bool hasError() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mErrorCode != ErrorCode::NoError;
+		}
+		
+		std::string errorMessage() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mErrorMessage;
+		}
+
+		const T& result() const {
+			std::lock_guard<std::mutex> lock(mMutex);
+			return mResult;
+		}
+		
+		void wait() const {
+			std::unique_lock<std::mutex> lock(mMutex);
+			while(!mIsReady) {
+				mDoneCondition.wait(lock);
+			}
+		}
+		
+		typedef std::function<void (T&)> OnDoneCallback;
+		typedef std::function<void ()> OnCancelledCallback;
+		typedef std::function<void (float)> OnProgressCallback;
+		typedef std::function<void (ErrorCode, const std::string&)> OnErrorCallback;
+		
+		void registerOnDoneCallback(OnDoneCallback callback)
+		{
+			std::unique_lock<std::mutex> lock(mMutex);
+			if(mIsReady) {
+				callback(mResult);
+			}
+			mOnDoneCallback = callback;
+		}
+		
+		void registerOnCancelledCallback(OnCancelledCallback callback)
+		{
+			std::unique_lock<std::mutex> lock(mMutex);
+			if(mErrorCode == ErrorCode::UserCancelled) {
+				callback();
+			}
+			mOnCancelledCallback = callback;
+		}
+		
+		void registerOnProgressCallback(OnProgressCallback callback)
+		{
+			std::unique_lock<std::mutex> lock(mMutex);
+			mProgressCallback = callback;
+			mProgressCallback(mProgress);
+		}
+		
+		void registerOnErrorCallback(OnErrorCallback callback)
+		{
+			std::unique_lock<std::mutex> lock(mMutex);
+			if(mErrorCode != ErrorCode::NoError) {
+				callback(mErrorCode, mErrorMessage);
+			}
+			mOnErrorCallback = callback;
+		}
+
 	private:
+		bool mIsDone;
+		bool mIsReady;
+		bool mCancelRequested;
+		T mResult;
 		float mProgress;
+		ErrorCode mErrorCode;
 		std::string mErrorMessage;
+		
+		OnDoneCallback mOnDoneCallback;
+		OnCancelledCallback mOnCancelledCallback;
+		OnProgressCallback mProgressCallback;
+		OnErrorCallback mOnErrorCallback;
+
+		std::mutex mMutex;
+		std::condition_variable mDoneCondition;
 	};
 	
 	template<typename T>
-	using AsyncProgressResult = std::shared_ptr<AsyncProgressData<T>>;
+	class AsyncProgress
+	{
+	public:
+		AsyncProgress(const AsyncProgressData<T>& data) : mData(data) { }
+		AsyncProgress(AsyncProgressData<T>&& data) : mData(std::move(data)) { }
+		
+		bool isDone() const { return mData->isDone(); }
+		bool isReady() const { return mData->isReady(); }
+		float progress() const { return mData->progress(); }
+		bool hasError() const { return mData->hasError(); }
+		std::string errorMessage() const { return mData->errorMessage(); }
+		
+		void wait() const { mData->wait(); }
+		
+		template<typename U>
+		AsyncProgress onDone(U func) {
+			mData->registerOnDoneCallback(func);
+			return *this;
+		}
+		
+		template<typename U>
+		AsyncProgress onCancelled(U func) {
+			mData->registerOnCancelledCallback(func);
+			return *this;
+		}
+		
+		template<typename U>
+		AsyncProgress onProgress(U func) {
+			mData->registerOnProgressCallback(func);
+			return *this;
+		}
+		
+		template<typename U>
+		AsyncProgress onError(U func) {
+			mData->registerOnErrorCallback(func);
+			return *this;
+		}
+		
+	private:
+		std::shared_ptr<AsyncProgressData<T>> mData;
+	};
 }
