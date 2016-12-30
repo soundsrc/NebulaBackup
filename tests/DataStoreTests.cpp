d93533e7f4d27349ac5de050d845cbd260fd43df
Sound <sound@sagaforce.com>
2016-12-30 13:08:11 -0800

    Updating datastore tests.

--- tests/DataStoreTests.cpp
+++ tests/DataStoreTests.cpp
@@ -13,8 +13,11 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <memory>
+#include <vector>
 #include <string.h>
 #include <boost/filesystem.hpp>
+#include "libnebula/DataStore.h"
 #include "libnebula/backends/FileDataStore.h"
 #include "libnebula/MemoryInputStream.h"
 #include "libnebula/MemoryOutputStream.h"
@@ -22,7 +25,7 @@
 #include "libnebula/ScopedExit.h"
 #include "gtest/gtest.h"
 
-TEST(DataStoreTests, Put) {
+TEST(DataStoreTests, PutAndGet) {
 
 	using namespace boost::filesystem;
 	using namespace Nebula;
@@ -34,8 +37,7 @@ TEST(DataStoreTests, Put) {
 		ScopedExit onExit([tmpPath] { remove_all(tmpPath); });
 
 		FileDataStore ds(tmpPath.c_str());
-		
-		printf("%s\n", tmpPath.c_str());
+
 		uint8_t data1[4096];
 		uint8_t data2[4096];
 		
@@ -43,13 +45,111 @@ TEST(DataStoreTests, Put) {
 		arc4random_buf(data2, sizeof(data2));
 		
 		MemoryInputStream dataStream(data1, sizeof(data1));
-		ds.put("/dddddd", dataStream).wait();
+		AsyncProgress<bool> progress = ds.put("/dddddd", dataStream);
+		progress.wait();
+		EXPECT_FALSE(progress.hasError());
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_TRUE(progress.result());
 		
 		MemoryOutputStream outStream(data2, sizeof(data2));
-		ds.get("/dddddd", outStream);
+		progress = ds.get("/dddddd", outStream);
+		progress.wait();
+		EXPECT_FALSE(progress.hasError());
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_TRUE(progress.result());
 		
 		EXPECT_TRUE(memcmp(data1, data2, sizeof(data1)) == 0);
 	}
 
+	EXPECT_FALSE(exists(tmpPath));
+}
+
+TEST(DataStoreTests, PutAndGetWithSubPaths) {
+	
+	using namespace boost::filesystem;
+	using namespace Nebula;
+	
+	path tmpPath = unique_path();
+	EXPECT_TRUE( create_directory(tmpPath) );
+	
+	{
+		ScopedExit onExit([tmpPath] { remove_all(tmpPath); });
+		
+		FileDataStore ds(tmpPath.c_str());
+		
+		uint8_t data1[4096];
+		uint8_t data2[4096];
+		
+		arc4random_buf(data1, sizeof(data1));
+		arc4random_buf(data2, sizeof(data2));
+		
+		MemoryInputStream dataStream(data1, sizeof(data1));
+		AsyncProgress<bool> progress = ds.put("/subpath/dddddd", dataStream);
+		progress.wait();
+		EXPECT_FALSE(progress.hasError());
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_TRUE(progress.result());
+		
+		MemoryOutputStream outStream(data2, sizeof(data2));
+		progress = ds.get("/subpath/dddddd", outStream);
+		progress.wait();
+		EXPECT_FALSE(progress.hasError());
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_TRUE(progress.result());
+		
+		EXPECT_TRUE(memcmp(data1, data2, sizeof(data1)) == 0);
+	}
+	
+	EXPECT_FALSE(exists(tmpPath));
+}
+
+TEST(DataStoreTests, GetNotFound) {
+	using namespace boost::filesystem;
+	using namespace Nebula;
+	
+	{
+		FileDataStore ds(".");
+
+		uint8_t data[12];
+		
+		MemoryOutputStream outStream(data, sizeof(data));
+		AsyncProgress<bool> progress = ds.get("/notfound", outStream);
+		progress.wait();
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_FALSE(progress.result());
+	}
+}
+
+TEST(DataStoreTests, GetNotEnoughSpace) {
+	using namespace boost::filesystem;
+	using namespace Nebula;
+	
+	path tmpPath = unique_path();
+	EXPECT_TRUE( create_directory(tmpPath) );
+	
+	{
+		ScopedExit onExit([tmpPath] { remove_all(tmpPath); });
+		
+		FileDataStore ds(tmpPath.c_str());
+		
+		uint8_t data1[4096];
+		uint8_t data2[12];
+		
+		arc4random_buf(data1, sizeof(data1));
+		arc4random_buf(data2, sizeof(data2));
+		
+		MemoryInputStream dataStream(data1, sizeof(data1));
+		AsyncProgress<bool> progress = ds.put("/ewfewfe", dataStream);
+		progress.wait();
+		EXPECT_TRUE(progress.isReady());
+		EXPECT_TRUE(progress.result());
+		
+		MemoryOutputStream outStream(data2, sizeof(data2));
+		progress = ds.get("/ewfewfe", outStream);
+		progress.wait();
+		EXPECT_FALSE(progress.isReady());
+		EXPECT_TRUE(progress.hasError());
+	}
+	
 	EXPECT_FALSE(exists(tmpPath));
 }
